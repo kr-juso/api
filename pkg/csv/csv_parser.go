606d31a8057652f14dc014dee237db39d3381eca
YeongCheon Kim <kyc1682@gmail.com>
2021-10-19 00:45:49 +0900

    [BUG] codePattern 길이가 0일 경우 panic 발생하는 문제 수정

--- pkg/csv/csv_parser.go
+++ pkg/csv/csv_parser.go
@@ -4,8 +4,8 @@ import (
 	"bytes"
 	_ "embed"
 	"encoding/csv"
-
 	"github.com/kr-juso/api/pkg/model"
+	"strings"
 )
 
 //go:embed internal/address.csv
@@ -32,4 +32,25 @@ func init() {
 			Name: row[1],
 		}
 	}
+}
+
+// codePattern이 00000000* 패턴일 경우 strings.HasPrefix 체크
+// codePattern이 *00000000 패턴일 경우 strings.HasSuffix 체크
+func GetRegcodes(codePattern string) []model.Regcode {
+	result := make([]model.Regcode, 0)
+
+	for _, regcode := range regcodes {
+		isContains := false
+		if strings.HasPrefix(codePattern, "*") {
+			isContains = strings.HasSuffix(regcode.Code, strings.ReplaceAll(codePattern, "*", ""))
+		} else if strings.HasSuffix(codePattern, "*") {
+			isContains = strings.HasPrefix(regcode.Code, strings.ReplaceAll(codePattern, "*", ""))
+		}
+
+		if isContains {
+			result = append(result, regcode)
+		}
+	}
+
+	return result
 }
